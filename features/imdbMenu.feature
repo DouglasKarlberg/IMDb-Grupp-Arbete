8ef56d0be5e84fb4267b4a5c0f3eaa3bebdae042
Mohamad Daoud <mdaoud@Mohamads-MacBook-Air.local>
2020-05-15 18:59:17 +0200

    done with scenario: Open and then close menu

--- features/imdbMenu.feature
+++ features/imdbMenu.feature
@@ -5,3 +5,8 @@ Feature: Explore menu, submenus and its contents
     Given that I am on IMDbs website
     When I click the main menu
     And visit any title from the drop down menu
+
+  Scenario: When open the main menu, I shall close it
+    Given that I am on IMDbs website
+    When I click the main menu
+    And close the menu by clicking on 'X' button
--- step-definitions/imdbMenu.js
+++ step-definitions/imdbMenu.js
@@ -18,8 +18,15 @@ module.exports = function () {
 
   this.When(/^visit any title from the drop down menu$/, async function () {
     let dropMenuClicked = await driver.wait(until.elementLocated(By.linkText('Toronto Int\'l Film Festival')));  //assign the link text by using elemntLocated of drop-menu to dropMenuClicked
-    await dropMenuClicked.click();                                                                               //drop menu title is clicked by calling click method
-    //await sleep(2000);                                                                                          //set sleep timer to 2 seconds
+    await dropMenuClicked.click();                                                                               //drop menu title is clicked by calling click method                                                                                         //set sleep timer to 2 seconds
+  });
+
+  //================= SCENARIO: 'When open the main menu, I shall close it' =================
+
+  this.When(/^close the menu by clicking on 'X' button$/, async function () {
+    let clickedXButton = await await driver.findElement(By.css
+      ('label.ipc-icon-button.BoMErbIIt00tb_ZoTOWZv.ipc-icon-button--baseAlt.ipc-icon-button--onBase'));   //assigns css element by using findElement to the variable clickedXButton
+    expect(clickedXButton, 'X Button was not found');                                                      //throws an error if css element was not found by using expect
+    await clickedXButton.click();                                                                          //click method, when click, closes the main menu
   });
 }
-
