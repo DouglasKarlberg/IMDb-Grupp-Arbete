d6d397e46a6afe0ae5fc2794acd5a77e43b68b26
DouglasKarlberg <DouglasKarlberg@protonmail.com>
2020-05-19 11:55:11 +0200

    Improvements for imdbSearch

--- step-definitions/imdbSearch.js
+++ step-definitions/imdbSearch.js
@@ -7,6 +7,7 @@ module.exports = function () {
   let sleepTime = 0;
 
   //================= SCENARIO: 'Searching for something that doesn't exist using click' BEGINS =================
+  
   //Given that I am on IMDb's website
   this.Given(/^that I am on IMDbs website$/, async function () {
     await helpers.loadPage("https://www.imdb.com"); //'Load a URL, returning only when the <body> tag is present' - npmjs.com
@@ -16,10 +17,10 @@ module.exports = function () {
   //When I type in search text "äpdqwjeha"
   this.When(/^I type in search text "([^"]*)"$/, async function (badInputText) {
     let imdbSearchField = await $('input[placeholder="Search IMDb"]'); //'imdbSearchField' receives document.queryslector('input[placeholder="Search IMDb"')
-    //input[placeholder="Search IMDb"] is the placeholder text within the search window
+                                                                       //input[placeholder="Search IMDb"] is the placeholder text within the search window
     expect(imdbSearchField, "Search Field was not found"); //Expect imdbSearchField to exist/be true. If not; throw error 'Search Field was not found'
     imdbSearchField.sendKeys(badInputText); //SendKeys will type in the given letters from 'badInputText' into imdbSearchField
-    //which is input[placeholder="Search IMDb"]
+                                            //which is input[placeholder="Search IMDb"]
     await sleep(sleepTime); //sleep(sleepTime) delays this step from closing based on the time(value) 'sleepTime' has
   });
 
@@ -31,12 +32,8 @@ module.exports = function () {
   });
 
   //Then the search result should contain the text message "No results found for "
-  this.Then(
-    /^the search result should contain the text message "([^"]*)"$/,
-    async function (noResult) {
-      await driver.wait(
-        until.elementLocated(By.css(".findResult, .findNoResults"))
-      ); //Wait until element .findResult or .findNoResults has been located
+  this.Then(/^the search result should contain the text message "([^"]*)"$/, async function (noResult) {
+      await driver.wait(until.elementLocated(By.css(".findResult, .findNoResults"))); //Wait until element .findResult or .findNoResults has been located
       let findNoResults = await $(".findNoResults"); //'findNoResults' receives div.findNoResults
       let h1 = await $("h1.findHeader"); //'h1' receives document.queryselector('h1.findHeader')
       let noResultsFound = await h1.getText(); //'noResultsFound' receives the text content within h1 with help of 'getText()' method
@@ -56,9 +53,7 @@ module.exports = function () {
   /////////////////////////////////////
 
   //When I type search text "löäkpälkadawjd" and presses ENTER
-  this.When(/^I type search text "([^"]*)" \+ ENTER$/, async function (
-    badInputText2
-  ) {
+  this.When(/^I type search text "([^"]*)" \+ ENTER$/, async function (badInputText2) {
     let imdbSearchField = await $('input[placeholder="Search IMDb"]'); //'imdbSearchField' receives document.queryselector('input[placeholder="Search IMDb"]')
     expect(imdbSearchField, "Search Field was not found"); //Expect imdbSearchField to exist/be true
     imdbSearchField.sendKeys(badInputText2); //SendKeys will type in the given letters from 'badInputText2' into imdbSearchField
@@ -67,11 +62,8 @@ module.exports = function () {
 
   //Then the search result should contain the message "No results found for "
   this.Then(
-    /^the search result should contain the message "([^"]*)"$/,
-    async function (noResult) {
-      await driver.wait(
-        until.elementLocated(By.css(".findResult, .findNoResults"))
-      ); //Wait until element .findResult or .findNoResults has been located
+    /^the search result should contain the message "([^"]*)"$/, async function (noResult) {
+      await driver.wait(until.elementLocated(By.css(".findResult, .findNoResults"))); //Wait until element .findResult or .findNoResults has been located
       let findNoResults = await $(".findNoResults"); //'findNoResults' receives div.findNoResults
       let h1 = await $("h1.findHeader"); //'h1' receives document.queryselector('h1.findHeader')
       let noResultsFound = await h1.getText(); //'noResultsFound' receives the text content within h1 with help of 'getText()' method
@@ -85,6 +77,10 @@ module.exports = function () {
   //================= SCENARIO: 'Searching for something that doesn't exist using ENTER' ENDS =================
 
   //================= SCENARIO: 'Navigate search suggestions and then press ENTER to load given suggestion' BEGINS =================
+
+  ///////////////////////////////////
+  //Given that I am on IMDbs website
+  ///////////////////////////////////
 
   //When I type search text "The Shining"
   this.When(/^I type search text "([^"]*)"$/, async function (inputText) {
@@ -105,9 +101,7 @@ module.exports = function () {
   });
 
   //Then I should be taken to the detail page of "The Shining"
-  this.Then(
-    /^I should be taken to the detail page of "([^"]*)"$/,
-    async function (inputText) {
+  this.Then(/^I should be taken to the detail page of "([^"]*)"$/, async function (inputText) {
       await driver.wait(until.elementLocated(By.css(".title_wrapper"))); //Wait for element css '.title_wrapper' to be located (basically just wait for it to load)
       //This wait method is used because it's not a garantee that the detail page will load at
       //the same speed on different computers, so use this is a better alternative to sleep mehtod
@@ -118,24 +112,29 @@ module.exports = function () {
     }
   );
 
-  // That I am on IMDB and search for John Wick
+  //================= SCENARIO: 'Navigate search suggestions and then press ENTER to load given suggestion' ENDS =================
+
+  //================= SCENARIO: 'Search for an existing movies' BEGINS =================
+
+  ///////////////////////////////////
+  //Given that I am on IMDbs website
+  ///////////////////////////////////
 
   //I search for existing movie Johan Wick
-  this.When(/^I search for an existing movie "([^"]*)"$/, async function (
-    inputText2
-  ) {
+  this.When(/^I search for an existing movie "([^"]*)"$/, async function (inputText2) {
     let imdbSearch2 = await $('input[placeholder="Search IMDb"]');
     expect(imdbSearch2, "Didn´t find search field").to.exist;
     await imdbSearch2.sendKeys(inputText2);
-    await sleep(5000);
+    await sleep(sleepTime);
   });
 
+  ///////////////////////////////////
+  //And click the search button
+  ///////////////////////////////////
+
   //Then search result should show detail page of John Wick
-  this.Then(
-    /^the search result should show result of "([^"]*)"$/,
-    async function (inputText) {
+  this.Then(/^the search result should show result of "([^"]*)"$/, async function (inputText) {
       await driver.wait(until.elementLocated(By.css('.findResult, .findNoResults')));
-      // now the search has finisehd
       let results = await $('.findResult');
       assert(results, 'Could not find any results');
       let firstResult = results[0];
@@ -145,5 +144,5 @@ module.exports = function () {
     }
   );
 
-  //================= SCENARIO: 'Navigate search suggestions and then press ENTER to load given suggestion' ENDS =================
+  //================= SCENARIO: 'Search for an existing movies' ENDS =================
 };
