3ed372dba246aac367760bfe13c6403a0c76e918
DouglasKarlberg <DouglasKarlberg@protonmail.com>
2020-05-19 11:43:40 +0200

    improvements for imdbCookie

--- step-definitions/imdbCookie.js
+++ step-definitions/imdbCookie.js
@@ -8,32 +8,42 @@ module.exports = function () {
 
   this.Given(/^that I load detail's page for a film$/, async function () {
     await helpers.loadPage('https://www.imdb.com/title/tt6751668/?ref_=hm_rvi_tt');       //Load a chosen movie detail's page
-    await sleep(2000);                                                                    //delays here is set to 2 seconds
-
+    await sleep(sleepTime);                                                                    //delays here is set to 2 seconds
   });
 
   this.When(/^clicking on 'IMDb' brand logo to load startpage$/, async function () {
+    await driver.wait(until.elementLocated(By.css('svg[id="home_img"]')));
     let logoButton = await driver.findElement(By.css('#home_img_holder'));    //assigns css element by using findElement to variable logoButton
     expect(logoButton, 'Logo button was not found');                          //throws an error if css element was not found by using expect
     await logoButton.click();                                                 //click method, when click, redirects page to the start page
-    await sleep(5000);                                                        //delays here is set to 5 seconds
+    await sleep(sleepTime);                                                   
   });
 
   this.Then(/^by clicking 'Clear all', all recently viewed will be cleared$/, async function () {
+    await driver.wait(until.elementLocated(By.css('div[class="recently-viewed"]')));
+    let pagebody = await $('body');
+    pagebody.sendKeys(selenium.Key.PAGE_DOWN);
+    await sleep(200);
+    pagebody.sendKeys(selenium.Key.PAGE_DOWN);
     let clearTextButton = await driver.wait(until.elementLocated(By.linkText('Clear all')));  //assign the link text by using elemntLocated to variable clearTextButton
     expect(clearTextButton, 'clear all button was not found');                                //throws an error if element was not found by using expect
     await clearTextButton.click();                                                            //click method, when click, clear what has been viewed recently from the list of 'Recently viewed'
-    await sleep(3000);                                                                        //delays here is set to 3 seconds
+    await sleep(sleepTime);                                                                        //delays here is set to 3 seconds
   });
 
   //================= SCENARIO: 'Visit a recently viewed movie' =================
 
   this.Then(/^click on the card under 'Recently viewed' for the movie's page you just left$/, async function () {
+    let pagebody = await $('body');
+    pagebody.sendKeys(selenium.Key.PAGE_DOWN);
+    await sleep(200);
+    pagebody.sendKeys(selenium.Key.PAGE_DOWN);
+    await sleep(200);
+    await driver.wait(until.elementLocated(By.css('div[class="recently-viewed"]')));
     let recentViewdButton = await driver.wait(until.elementLocated(By.linkText('Gisaengchung')));   //assign the link text by using elemntLocated to variable recentViewdButton
     expect(recentViewdButton, 'Recent Viewed button was not found');                                //throws an error if css element was not found by using expect
     await recentViewdButton.click();                                                                //click method, when click, redirects to the viewed recently for movie detail's page I just visited
-    await sleep(5000);                                                                              //delays here is set to 5 seconds
-
+    await sleep(sleepTime);
+    await driver.wait(until.elementLocated(By.css('div[class="title_block"]')));                                                               
   });
-
 }
